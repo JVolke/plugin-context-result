6eef8b706727a96daf4dc6dd7e8b84d61d9edbe9
JVolke <desire.add@gmail.com>
2018-06-16 12:04:52 +0200

    add Config for ResultFields

--- meta/documents/user_guide_de.md
+++ meta/documents/user_guide_de.md
@@ -1,3 +1,19 @@
 # Plugin Context Result
 
-Dieses Plugin erlaubt es die Context-Klassen von Ceres zu erweitern und ResultFields zu überschreiben. Das Plugin richtet sich dabei an alle, die CeresVanilla direkt vom Marketplace nutzen und keine Möglichkeit haben eigene Dateien zum Plugin hinzuzufügen. Generell kann dieses Plugin aber mit jedem Theme verwendet werden. 
+Dieses Plugin erlaubt es die Context-Klassen von Ceres zu erweitern und ResultFields zu überschreiben. Das Plugin richtet sich dabei an alle, die CeresVanilla direkt vom Marketplace nutzen und keine Möglichkeit haben eigene Dateien zum Plugin hinzuzufügen. Generell kann dieses Plugin aber mit jedem Theme verwendet werden.
+
+## Wie richte ich das Plugin ein?
+Zuerst muss das Plugin hier im Marketplace "gekauft" werden. Anschließend sollte das Plugin unter *Plugin => Plugin Übersicht* angezeigt werden, wenn Ihr in den Filter Einstellungen des aktuellen Plugin-Sets folgendes Auswählt
+- Herkunft = Marketplace
+- Installiert = Nicht Installiert
+
+Nun kann man in der Spalte Aktion auf *Plugin Installieren* klicken, damit das Plugin installiert wird.
+
+Wurde das Plugin installiert sollte für diese Plugin die Position festgelegt werden. Die Position sollte dabei so gewählt werden, dass dieses Plugin zwischen IO und Eurem Theme sich einordnet. Beispielsweise könnte Ihr folgende Positionen verwenden:
+
+- IO = 999
+- Ceres = 910
+- Euer Theme-Plugin = 911
+- ContextResult = 998
+
+Nun muss das Plugin-Set noch einmal gespeichert werden, damit das Plugin bereitgestellt wird.
--- src/Providers/ContextResultServiceProvider.php
+++ src/Providers/ContextResultServiceProvider.php
@@ -6,6 +6,7 @@ use Plenty\Plugin\ServiceProvider;
 use Plenty\Plugin\Events\Dispatcher;
 use IO\Helper\TemplateContainer;
 use IO\Helper\ComponentContainer;
+use Plenty\Plugin\ConfigRepository;
 use IO\Services\ItemSearch\Helper\ResultFieldTemplate;
 
 /**
@@ -27,25 +28,35 @@ class ContextResultServiceProvider extends ServiceProvider
 
     }
 
-    public function boot(Dispatcher $dispatcher)
+    public function boot(Dispatcher $dispatcher, ConfigRepository $config)
     {
-          $dispatcher->listen( 'IO.ResultFields.*', function(ResultFieldTemplate $templateContainer) {
-             $templateContainer->setTemplates([
-                 ResultFieldTemplate::TEMPLATE_SINGLE_ITEM   => 'ContextResult::ResultFields.SingleItem'
-             ]);
-          }, 0);
-
-         $dispatcher->listen( 'IO.ResultFields.*', function(ResultFieldTemplate $templateContainer) {
+          $enabledOverrides = explode(", ", $config->get("ContextResult.fields.override"));
+          if (in_array("single_item", $enabledOverrides) || in_array("all", $enabledOverrides))
+          {
+            $dispatcher->listen( 'IO.ResultFields.*', function(ResultFieldTemplate $templateContainer) {
                $templateContainer->setTemplates([
-                   ResultFieldTemplate::TEMPLATE_LIST_ITEM   => 'ContextResult::ResultFields.ListItem'
+                   ResultFieldTemplate::TEMPLATE_SINGLE_ITEM   => 'ContextResult::ResultFields.SingleItem'
                ]);
-         }, 0);
+            }, 0);
+          }
 
-         $dispatcher->listen( 'IO.ResultFields.*', function(ResultFieldTemplate $templateContainer) {
+          if (in_array("list_item", $enabledOverrides) || in_array("all", $enabledOverrides))
+          {
+           $dispatcher->listen( 'IO.ResultFields.*', function(ResultFieldTemplate $templateContainer) {
                  $templateContainer->setTemplates([
-                     ResultFieldTemplate::TEMPLATE_BASKET_ITEM    => 'ContextResult::ResultFields.BasketItem'
+                     ResultFieldTemplate::TEMPLATE_LIST_ITEM   => 'ContextResult::ResultFields.ListItem'
                  ]);
-         }, 0);
+           }, 0);
+         }
+
+         if (in_array("basket_item", $enabledOverrides) || in_array("all", $enabledOverrides))
+         {
+           $dispatcher->listen( 'IO.ResultFields.*', function(ResultFieldTemplate $templateContainer) {
+                   $templateContainer->setTemplates([
+                       ResultFieldTemplate::TEMPLATE_BASKET_ITEM    => 'ContextResult::ResultFields.BasketItem'
+                   ]);
+           }, 0);
+         }
 
          $dispatcher->listen('IO.ctx.item', function (TemplateContainer $templateContainer, $templateData = [])
           {
