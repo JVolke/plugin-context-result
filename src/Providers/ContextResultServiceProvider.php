f7bf4bc7efa5a672b686ccfed9b6ebda6e833765
JVolke <desire.add@gmail.com>
2018-06-16 09:47:52 +0200

    Plugin Create - Basic for Override ResultFields

---
+++ src/Providers/ContextResultServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ContextResult\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Events\Dispatcher;
+use IO\Helper\TemplateContainer;
+use IO\Helper\ComponentContainer;
+use IO\Services\ItemSearch\Helper\ResultFieldTemplate;
+
+/**
+ * Class ContextResultServiceProvider
+ * @package ContextResult\Providers
+ */
+class ContextResultServiceProvider extends ServiceProvider
+{
+    /**
+    * Register the route service provider
+    */
+    public function register()
+    {
+
+    }
+
+    public function boot(Dispatcher $dispatcher)
+    {
+          $dispatcher->listen( 'IO.ResultFields.*', function(ResultFieldTemplate $templateContainer) {
+             $templateContainer->setTemplates([
+                 ResultFieldTemplate::TEMPLATE_SINGLE_ITEM   => 'ContextResult::ResultFields.SingleItem'
+             ]);
+          }, 0);
+
+         $dispatcher->listen( 'IO.ResultFields.*', function(ResultFieldTemplate $templateContainer) {
+               $templateContainer->setTemplates([
+                   ResultFieldTemplate::TEMPLATE_LIST_ITEM   => 'ContextResult::ResultFields.ListItem'
+               ]);
+         }, 0);
+
+         $dispatcher->listen( 'IO.ResultFields.*', function(ResultFieldTemplate $templateContainer) {
+                 $templateContainer->setTemplates([
+                     ResultFieldTemplate::TEMPLATE_BASKET_ITEM    => 'ContextResult::ResultFields.BasketItem'
+                 ]);
+         }, 0);
+
+    }
+
+}
