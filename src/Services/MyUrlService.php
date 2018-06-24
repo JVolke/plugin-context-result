91f80843ddeee9241ed15f87d70b55ff5696c419
JVolke <desire.add@gmail.com>
2018-06-24 12:30:36 +0200

    TRy to extend IO class

--- src/Services/MyUrlService.php
+++ src/Services/MyUrlService.php
@@ -7,8 +7,9 @@ use IO\Services\UrlBuilder\CategoryUrlBuilder;
 use IO\Services\UrlBuilder\UrlQuery;
 use ContextResult\Services\UrlBuilder\VariationUrlBuilder;
 use Plenty\Plugin\Http\Request;
+use IO\Services\UrlService;
 
-class MyUrlService
+class MyUrlService extends UrlService
 {
     use MemoryCache;
 
