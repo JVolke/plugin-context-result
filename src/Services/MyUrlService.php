27f6643b808d80675c4abac0fe72b0aa76bc7395
JVolke <desire.add@gmail.com>
2018-06-24 12:40:09 +0200

    Test

--- src/Services/MyUrlService.php
+++ src/Services/MyUrlService.php
@@ -11,5 +11,32 @@ use IO\Services\UrlService;
 
 class MyUrlService extends UrlService
 {
-    
+  parent::getVariationURL( $itemId, $variationId, $lang = null )
+  {
+      if ( $lang === null )
+      {
+          $lang = pluginApp( SessionStorageService::class )->getLang();
+      }
+
+      $variationUrl = $this->fromMemoryCache(
+          "variationUrl.$itemId.$variationId.$lang",
+          function() use ($itemId, $variationId, $lang) {
+              /** @var VariationUrlBuilder $variationUrlBuilder */
+              $variationUrlBuilder = pluginApp( VariationUrlBuilder::class );
+              $variationUrl = $variationUrlBuilder->buildUrl( $itemId, $variationId, $lang );
+
+              if ( $variationUrl->getPath() !== null )
+              {
+                  $variationUrl->append(
+                      $variationUrlBuilder->getSuffix( $itemId, $variationId )
+                  );
+              }
+
+              return $variationUrl;
+          }
+      );
+
+      return $variationUrl;
+  }
+
 }
