3a6bbce9a415dfacae0122b4b9b310d077dce20c
JVolke <desire.add@gmail.com>
2018-07-26 07:24:46 +0200

    Testing

--- src/Contexts/ContextResultSingleItemContext.php
+++ src/Contexts/ContextResultSingleItemContext.php
@@ -7,6 +7,7 @@ use Ceres\Contexts\SingleItemContext;
 use Plenty\Modules\Item\Item\Contracts\ItemRepositoryContract;
 use IO\Services\ItemSearch\Services\ItemSearchService;
 use IO\Services\ItemSearch\SearchPresets\CrossSellingItems;
+use Plenty\Modules\Item\Variation\Contracts\VariationRepositoryContract;
 use Plenty\Plugin\Log\Loggable;
 
 class ContextResultSingleItemContext extends SingleItemContext implements ContextInterface
@@ -27,6 +28,10 @@ class ContextResultSingleItemContext extends SingleItemContext implements Contex
     $searchfactory->setPage(1,4);
     $result = pluginApp(ItemSearchService::class)->getResult( $searchfactory );
     $this->getLogger(__METHOD__)->error("Debug Cross Selling", $result);
+    $with = [ "variationProperties" => 1 ];
+    $land = "de";
+    $mainVariationProperties = pluginApp(VariationRepositoryContract::class)->show($this->item['documents'][0]['data']['item']['id'], $with, $lang);
+    $this->getLogger(__MEHOD__)->error("Debuggin", $mainVariationProperties);
 
   }
 }
